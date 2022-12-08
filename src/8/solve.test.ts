9401681e30dd3e972c3581ce1ae179787536c5a4
Anders Gustafsson <andersgee@gmail.com>
2022-12-08 07:13:10 +0100

    8 b

--- src/8/solve.test.ts
+++ src/8/solve.test.ts
@@ -3,7 +3,7 @@ import { parse, type Puzzle, log } from "../puzzle.ts";
 
 const FILE_PATH = "./src/8/input.txt";
 const GOLD_A = 21;
-const GOLD_B = 1;
+const GOLD_B = 8;
 const TEST_INPUT = `30373
 25512
 65332
@@ -11,9 +11,6 @@ const TEST_INPUT = `30373
 35390`;
 
 function isvisible(x: number, y: number, M: number[][]) {
-  const szY = M.length;
-  const szX = M[0].length;
-
   const h = M[y][x];
   const row = M[y];
   const left = row.slice(0, x);
@@ -24,10 +21,10 @@ function isvisible(x: number, y: number, M: number[][]) {
   //log({ h, left, right, seenFromLeft, seenFromRight });
 
   const col = M.map((row) => row[x]);
-  log({ col });
+  //log({ col });
   const top = col.slice(0, y);
   const bot = col.slice(y + 1);
-  log({ x, y, bot });
+  //log({ x, y, bot });
   const seenFromTop = top.every((x) => x < h) || top.length < 1;
   const seenFromBot = bot.every((x) => x < h) || bot.length < 1;
 
@@ -38,23 +35,52 @@ function isvisible(x: number, y: number, M: number[][]) {
     return true;
   }
 
-  /*
-  for (let i = 0; i < szX; i++) {
-    //from top
-    for (let j = 0; j < y; j++) {
-      if (M[j][i]! >= h) return false;
-    }
-    //from bottom
-    for (let j = szY - 1; j >= y; j--) {
-      //log("LALAL M[j][i]", M[j][i]);
-      if (M[j][i]! >= h) return false;
-    }
-  }
-  log("visible: x,y", x, y);
-  */
   return false;
 }
 
+function scenicScore(x: number, y: number, M: number[][]) {
+  const h = M[y][x];
+
+  const row = M[y];
+  const left = row.slice(0, x).reverse();
+  const right = row.slice(x + 1);
+
+  const col = M.map((row) => row[x]);
+  const top = col.slice(0, y).reverse();
+  const bot = col.slice(y + 1);
+
+  let topScore = 0;
+  let botScore = 0;
+  let leftScore = 0;
+  let rightScore = 0;
+
+  for (const i of top) {
+    topScore++;
+    if (i >= h) {
+      break;
+    }
+  }
+  for (const i of bot) {
+    botScore++;
+    if (i >= h) {
+      break;
+    }
+  }
+  for (const i of right) {
+    rightScore++;
+    if (i >= h) {
+      break;
+    }
+  }
+  for (const i of left) {
+    leftScore++;
+    if (i >= h) {
+      break;
+    }
+  }
+
+  return topScore * botScore * leftScore * rightScore;
+}
 function solve_a(puzzle: Puzzle) {
   const M = puzzle.input
     .trim()
@@ -77,22 +103,36 @@ function solve_a(puzzle: Puzzle) {
 }
 
 function solve_b(puzzle: Puzzle) {
-  const res = 0;
-  return res;
+  const M = puzzle.input
+    .trim()
+    .split("\n")
+    .map((x) => x.split("").map(parseFloat));
+
+  //log({ M });
+  //log("M[0]", M[0]);
+  let scores = [];
+  for (let y = 0; y < M.length; y++) {
+    for (let x = 0; x < M[0].length; x++) {
+      const score = scenicScore(x, y, M);
+      scores.push(score);
+    }
+  }
+
+  return Math.max(...scores);
 }
 
+/*
 Deno.test("A", async () => {
   const testpuzzle = await parse({ input: TEST_INPUT });
   const res = solve_a(testpuzzle);
   log("A RESULT", solve_a(await parse({ filepath: FILE_PATH })));
   assertEquals(res, GOLD_A);
 });
+*/
 
-/*
 Deno.test("B", async () => {
   const testpuzzle = await parse({ input: TEST_INPUT });
   const res = solve_b(testpuzzle);
   log("B RESULT", solve_b(await parse({ filepath: FILE_PATH })));
   assertEquals(res, GOLD_B);
 });
-*/
