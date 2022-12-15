1b420e7735515b6fbd9d01848a3db71e51c50d28
andy <andersgee@gmail.com>
2022-12-15 23:00:11 +0100

    15 b

--- src/15/solve.test.ts
+++ src/15/solve.test.ts
@@ -3,7 +3,7 @@ import { log, parse, type Puzzle } from "../puzzle.ts";
 
 const FILE_PATH = "./src/15/input.txt";
 const GOLD_A = 26;
-const GOLD_B = 1;
+const GOLD_B = 56000011;
 const TEST_INPUT = `Sensor at x=2, y=18: closest beacon is at x=-2, y=15
 Sensor at x=9, y=16: closest beacon is at x=10, y=16
 Sensor at x=13, y=2: closest beacon is at x=15, y=3
@@ -55,121 +55,95 @@ function manhattanDist(from: Coord, to: Coord) {
 	return Math.abs(to[0] - from[0]) + Math.abs(to[1] - from[1]);
 }
 
-function coveredCoords(sensor: Sensor) {
-	const topleft = [
-		sensor.pos[0] - sensor.distToBeacon,
-		sensor.pos[1] - sensor.distToBeacon,
-	];
-	const botright = [
-		sensor.pos[0] + sensor.distToBeacon,
-		sensor.pos[1] + sensor.distToBeacon,
-	];
-	const coords: Coord[] = [];
-	log(`calculating coords with distToBeacon:${sensor.distToBeacon}`);
-	for (let y = topleft[1]; y < botright[1]; y++) {
-		for (let x = topleft[0]; x < botright[0]; x++) {
-			if (manhattanDist(sensor.pos, [x, y]) <= sensor.distToBeacon) {
-				coords.push([x, y]);
-			}
-		}
+function xvalsAtY(sensor: Sensor, y: number) {
+	const dy = Math.abs(y - sensor.pos[1]);
+	const dx = Math.abs(sensor.distToBeacon - dy);
+	if ((dy + dx) > sensor.distToBeacon) {
+		return [undefined, undefined];
 	}
-	return coords;
+	const left = sensor.pos[0] - dx;
+	const right = sensor.pos[0] + dx;
+	return [left, right];
 }
 
-function coveredCoordsOnRow(sensor: Sensor, y: number) {
-	const topleft = [
-		sensor.pos[0] - sensor.distToBeacon,
-		sensor.pos[1] - sensor.distToBeacon,
-	];
-	const botright = [
-		sensor.pos[0] + sensor.distToBeacon,
-		sensor.pos[1] + sensor.distToBeacon,
-	];
-	const coords: Coord[] = [];
-	log(`calculating coords with distToBeacon:${sensor.distToBeacon}`);
-	for (let x = topleft[0]; x < botright[0]; x++) {
-		if (manhattanDist(sensor.pos, [x, y]) <= sensor.distToBeacon) {
-			coords.push([x, y]);
-		}
-	}
-	return coords;
+function freqcalc(x: number, y: number) {
+	return x * 4000000 + y;
 }
 
-function solve_a(puzzle: Puzzle, examinedRow: number) {
+function solve_b(puzzle: Puzzle, maxX: number, maxY: number) {
 	const sensors = parseSensorsBeacons(puzzle);
-	log({ sensors });
 
-	//const covered = new Set<string>();
-	const covering_examinedRow = new Set<string>();
-	for (const sensor of sensors) {
-		log(`processing sensor at ${sensor.pos[0]}-${sensor.pos[1]}`);
-		const coords = coveredCoordsOnRow(sensor, examinedRow);
-		log("now have coords");
-		for (const coord of coords) {
-			const str = `${coord[0]}-${coord[1]}`;
-			//covered.add(str);
-			if (coord[1] === examinedRow) {
-				covering_examinedRow.add(str);
+	for (let y = 0; y <= maxY; y++) {
+		const segments: Array<{ index: number; from: number; to: number }> = [];
+		let index = 0;
+		for (const sensor of sensors) {
+			const [left, right] = xvalsAtY(sensor, y);
+			if (left !== undefined && right !== undefined) {
+				segments.push({ index, from: left, to: right });
+				index += 1;
 			}
 		}
-	}
 
-	//remove any already occupied...
-	for (const sensor of sensors) {
-		const str1 = `${sensor.pos[0]}-${sensor.pos[1]}`;
-		covering_examinedRow.delete(str1);
+		const sortedSegments = segments.sort((a, b) => a.from - b.from);
 
-		const str2 = `${sensor.closestBeacon[0]}-${sensor.closestBeacon[1]}`;
-		covering_examinedRow.delete(str2);
-	}
-	//log({ covering_examinedRow, sz: covering_examinedRow.size });
+		const consumedInds = new Set<number>();
+		const limits = sortedSegments[0]; //initial
+		consumedInds.add(sortedSegments[0].index);
 
-	/*
-	let minX = Infinity;
-	let maxX = -Infinity;
-	let minY = Infinity;
-	let maxY = -Infinity;
-	for (const sensor of sensors) {
-		minX = Math.min(minX, sensor.pos[0] - sensor.distToBeacon);
-		maxX = Math.max(maxX, sensor.pos[0] + sensor.distToBeacon);
-
-		minY = Math.min(minY, sensor.pos[1] - sensor.distToBeacon);
-		maxY = Math.max(maxY, sensor.pos[1] + sensor.distToBeacon);
-	}
-
-	let nCovered = 0;
-	for (let x = minX; x < maxX; x++) {
-		const str = `${x}-${examinedRow}`;
-		if (covered.has(str)) {
-			nCovered += 1;
+		for (let i = 0; i < sortedSegments.length; i++) { //repeat this couple of times
+			for (const segment of sortedSegments) {
+				if (!consumedInds.has(segment.index) && segment.from <= limits.to) {
+					limits.to = Math.max(limits.to, segment.to);
+					consumedInds.add(segment.index);
+				}
+			}
 		}
+		if (limits.from > 0) {
+			const x = limits.from;
+			log(`found uncovered (limits.from) coord: ${x}-${y}`);
+			return freqcalc(x, y);
+		}
+
+		if (limits.to < maxX) {
+			const x = limits.to + 1;
+			log(`found uncovered (limits.to) coord : ${x}-${y}`);
+			return freqcalc(x, y);
+		}
+
+		//if multiple start at same "from": only keep the one with largest "to"
+		//log({ sortedSegments, consumedInds, limits });
+
+		/*
+		for (let x = 0; x <= maxX; x++) {
+			if (!covered[x]) {
+				log(`found uncovered at x-y: ${x}-${y}`);
+				return x * 4000000 + y;
+			}
+		}
+		*/
+
+		log(y);
 	}
-	//log({ covered, nCovered });
 
-	return nCovered;
-  */
-
-	return covering_examinedRow.size;
+	return 0;
 }
-
 /*
-function solve_b(puzzle: Puzzle) {
-  const res = 0;
-  return res;
-}
- */
 Deno.test("A", async () => {
 	const testpuzzle = await parse({ input: TEST_INPUT });
 	const res = solve_a(testpuzzle, 10);
 	assertEquals(res, GOLD_A);
 	log("A RESULT", solve_a(await parse({ filepath: FILE_PATH }), 2000000));
 });
+ */
 
-/*
 Deno.test("B", async () => {
-  const testpuzzle = await parse({ input: TEST_INPUT });
-  const res = solve_b(testpuzzle);
-  assertEquals(res, GOLD_B);
-  //log("B RESULT", solve_b(await parse({ filepath: FILE_PATH })));
+	const testpuzzle = await parse({ input: TEST_INPUT });
+	const res = solve_b(testpuzzle, 20, 20);
+
+	log(
+		"B RESULT",
+		solve_b(await parse({ filepath: FILE_PATH }), 4000000, 4000000),
+	);
+
+	assertEquals(res, GOLD_B);
 });
-*/
